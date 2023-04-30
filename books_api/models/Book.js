d0dd21509b60e2bdd1c03bf4b7f1c1c8c94ade26
SudipSinghKhati <sudipkhati2043@gmail.com>
2023-04-30 10:12:29 +0545

    reviews

--- books_api/models/Book.js
+++ books_api/models/Book.js
@@ -1,7 +1,18 @@
 
-const { default: mongoose } = require('mongoose');
-const express = require('mongoose');
+const mongoose = require('mongoose');
 
+
+
+//creating review scheman 
+const reviewSchema = new mongoose.Schema({
+    text:{
+        type: String,
+        required: true,
+        miniLength: 10
+    }
+})
+
+ 
 // create Schema  --> build strcture to store data
 const bookSchema = new mongoose.Schema({
 
@@ -12,11 +23,14 @@ const bookSchema = new mongoose.Schema({
     author: {
         type: String,
         default: 'Anonymous'
-    }
+    },
+    //invadaing review schema in book 
+    reviews:[reviewSchema] 
 }, {timestamps: true});  // timestamps --> track when the book is added/updated
 
 // we use model to use database not by Schema
 module.exports = mongoose.model('Book', bookSchema);
+
 
  // Book --> is collection in Singular but, Mongdb makes plural
 
--- books_api/routes/book_routes.js
+++ books_api/routes/book_routes.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 // import custom books
 let books = require('../data/books');
@@ -32,26 +31,26 @@ router.route('/')
             res.json(books);
         }
         catch (error) {
-            console.log(error)}
+            console.log(error);
+        }
 
     })
 
     // add a book in the books list
 
     .post((req, res, next) => {
-
-       Book.create(req.body)
-       .then((book) => res.status(201).json(book))
-       .catch((err) => next(err))
+        Book.create(req.body)
+            .then(book => res.status(201).json(book))
+            .catch(next);
     })
 
-    .put((req, res) => {
-        res.status(405).json({ "error": "PUT request is not allowed" });
+    .put((req, res, next) => {
+        res.status(405).json({ error: "PUT request is not allowed" });
     })
-    .delete((req, res, next) => {
-       Book.deleteMany()
-       .then(reply=> res.json(reply))
-       .catch(next)
+    .delete((req, res) => {
+        Book.deleteMany()
+            .then(() => res.status(201).json({ "message": "Deleted all successfully" }))
+            .catch(next);
     });
 
 
@@ -60,37 +59,156 @@ router.route('/:book_id')
 
     // get only specific book
     .get((req, res, next) => {
-      Book.findById(req.params.book_id)
-      .then((book)=> {
-        if(!book){
-            res.status(404).json({error: 'book not found'})
-        }
-        
-        res.json(book)})
-      .catch(next)
+        Book.findById(req.params.book_id)
+            .then(book => {
+                // send this error handling if the book is not found
+                if (!book) {
+                    res.status(404).json({ error: "Book not found" });
+                }
+                res.json(book);
+            })
+            .catch(next);
     })
 
     // update particular book
     .put((req, res, next) => {
         Book.findByIdAndUpdate(
-            req.params.book_id,
-            {$set: req.body},
-            {new: true}
-            )
-            .then(updated => res.json(updated))
+            req.params.book_id,  // find this id book
+            { $set: req.body },  // update the changed data
+            { new: true }  // return updated data not old one
+        )
+            .then(updatedBook => res.status(200).json(updatedBook))
             .catch(next)
-         
     })
 
     // delete particular book
     .delete((req, res, next) => {
-        Book.findByIdAndDelete(req.params.book_id)
-        .then(reply => res.status(204).end())
-        .catch(next)
+        Book.findByIdAndDelete(
+            req.params.book_id
+        )
+            .then((deletedBook) => {
+                if (!deletedBook) res.status(404).json({ "erorr": "No found" });
+                res.status(202).end();
+            })
+            .catch(err => {
+                console.log(err);
+                next(err);
+            })
     })
     .post((req, res) => {
-        res.status(405).json({ "error": "POST method is not allowed here" });
+        res.status(405).json({ error: "POST method is not allowed here" });
     });
+
+// routes for reviews
+
+router.route('/:book_id/reviews')
+    .get((req, res, next) => {
+        Book.findById(req.params.book_id)
+            .then(book => {
+                if (!book) res.status(404).json({ error: "Book not found" });
+                res.json(book.reviews);
+            })
+            .catch(next);
+    })
+    .post((req, res, next) => {
+        Book.findById(req.params.book_id)
+            .then(book => {
+                if (!book) res.status(404).json({ error: "Book not found" });
+
+                const review = {
+                    text: req.body.text
+                }
+
+                book.reviews.push(review);
+                // length of total 
+                const reviewLength = book.reviews.length;
+                book.save()
+                    .then((book) => res.status(201).json(book.reviews[reviewLength - 1]))
+                    .catch(next);
+            })
+
+            .catch(next);
+    })
+
+    .delete((req, res, next) => {
+        Book.findById(req.params.book_id)
+            .then(book => {
+                if (!book) res.status(400).json({ error: "Book not found" });
+
+                // empty the review list
+                book.reviews = [];
+
+                // save it
+                book.save()
+                    .then(book => res.status(204).end())
+                    .catch(next);
+            })
+            .catch(next);
+
+    })
+    .put((req, res) => {
+        res.status(405).json({ error: "PUT method is not allowed here" });
+    });
+
+router.route('/:book_id/reviews/:review_id')
+
+    .get((req, res, next) => {
+        Book.findById(req.params.book_id)
+            .then(book => {
+                if (!book) res.status(400).json({ error: "Book not found" });
+                // get review 
+                const review = book.reviews.id(req.params.review_id);
+                if (!review) res.status(400).json({ error: "Review not found" });
+
+                res.json(review);
+
+            })
+            .catch(next);
+    })
+    .post((req, res) => {
+        res.status(405).json({ error: "POST method is not allowed here" });
+    })
+    .put((req, res, next) => {
+        Book.findById(req.params.book_id)
+            .then(book => {
+                if (!book) res.status(400).json({ error: "Book not found" });
+
+                // update review by traversing 
+                book.reviews = book.reviews.map(singleReview => {
+                    // update the review whose id  matches  , _id is an object so, == is used instead of ===
+                    if (singleReview._id == req.params.review_id) {
+                        singleReview.text = req.body.text
+                    }
+                    return singleReview;
+                })
+
+                // since, reviews is not a model so it is not used for saving , instead of book which is a model 
+                book.save().then(book => {
+                    res.json(book.reviews.id(req.params.review_id));
+                })
+                    .catch(next);
+            })
+            .catch(next);
+    })
+
+    .delete((req, res, next) => {
+        Book.findById(req.params.book_id)
+            .then(book => {
+                if (!book) res.status(400).json({ error: "Book not found" });
+
+                // delete specific review only
+                book.reviews = book.reviews.filter(singleReview => {
+                    return singleReview._id != req.params.review_id;
+                })
+                // use save() when you create own algorithm to save without using methods of mongoose
+                book.save()
+                    .then(book => res.status(204).end())
+                    .catch(next);
+
+            })
+            .catch(next);
+    });
+
 
 
 // export it to use in other file
